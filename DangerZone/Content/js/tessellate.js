ea63312086a10a6f04fb6ef71d8d98261c1bfe7d
Klors <klors@klors.com>
2013-05-27 23:48:48 +0100

    Settable size, type and altimg

--- DangerZone/Content/js/tessellate.js
+++ DangerZone/Content/js/tessellate.js
@@ -10,17 +10,22 @@ var DangerZone;
                     var h = $(this).height();
                     var w = $(this).width();
                     var s = $(this).attr("src");
-                    $(this).wrap("<div class=\"tessellated\" style=\"height: " + h + "px; width: " + w + "px;\"></div>");
+                    var as = $(this).data("tessellate-altimg");
+                    var alt = (typeof as !== "undefined" && as !== null && as !== "" && s.lastIndexOf("/") !== -1) ? s.replace(s.substring(s.lastIndexOf("/") + 1), $(this).data("tessellate-altimg")) : s;
+                    var tt = $(this).data("tessellate-type") === "fixed";
+                    var ts = parseInt($(this).data("tessellate-size"), 10);
+                    var sz = (isNaN(ts) || ts === 0) ? ts = (tt ? 20 : 5) : ts;
+                    var squareSize = tt ? sz : ((h < w) ? Math.ceil(h / sz) : Math.ceil(w / sz));
+                    var baseSpeed = 30;
+                    var accelerator = 10;
+                    $(this).wrap("<div class=\"tessellated\" style=\"height: " + h + "px; width: " + w + "px;\" data-tessellate-srcimg=\"" + alt + "\" data-tessellate-altimg=\"" + s + "\"></div>");
                     var wrapper = $(this).parent();
-                    var delayStart = 0, delay;
-                    for(var i = 0; i < h; i += 20) {
-                        delay = 0;
-                        for(var j = 0; j < w; j += 20) {
-                            wrapper.append($("<div class=\"tile\" style=\"background-image: url(" + s + "); background-position: -" + j + "px -" + i + "px; -webkit-animation-delay:" + (delayStart + delay) + "ms;\"></div>"));
-                            delay += 50;
+                    var delayStart = 0, delay = 0, x = 0;
+                    for(var i = 0; i < h; i += squareSize , x += accelerator , delay = 0 , delayStart += baseSpeed + x) {
+                        for(var j = 0; j < w; j += squareSize , delay += baseSpeed) {
+                            wrapper.append($("<div class=\"tile\" style=\"background-image: url(" + s + "); background-position: -" + j + "px -" + i + "px; width: " + squareSize + "px; height: " + squareSize + "px; -webkit-animation-delay:" + (delayStart + delay) + "ms;\"></div>"));
                         }
                         wrapper.append("<br/>");
-                        delayStart += 50 + i;
                     }
                     $(this).remove();
                 });
@@ -34,13 +39,21 @@ var DangerZone;
                     if($(this).css("-webkit-animation-name") === "flipback") {
                         $(this).addClass("nodelay");
                         $(this).css({
+                            "background-image": "url(" + $(this).parent().data("tessellate-altimg") + ")",
                             "-webkit-animation-play-state": "running"
                         });
+                        $(this).parent();
+                    } else {
                     }
                 });
             });
         };
         Tessellation.prototype.Flip = function (selector) {
+            $(".tessellated").each(function (idx, obj) {
+                var a = $(this).data("tessellate-altimg");
+                $(this).data("tessellate-altimg", $(this).data("tessellate-srcimg"));
+                $(this).data("tessellate-srcimg", a);
+            });
             var s = (typeof selector !== "undefined" && selector !== null && selector !== "") ? selector + ">div.tile" : "div.tile";
             $(s).removeClass("nodelay");
             $(s).css({
--- DangerZone/Content/js/tessellate.ts
+++ DangerZone/Content/js/tessellate.ts
@@ -7,22 +7,27 @@ module DangerZone {
         init() {
             $(window).on("load.tessellate", function () {
                 $("img.tessellate").each(function (idx, obj) {
-                    var h = $(this).height();
-                    var w = $(this).width();
-                    var s = $(this).attr("src");
+                    var h: number = $(this).height();
+                    var w: number = $(this).width();
+                    var s: string = $(this).attr("src");
+                    var as: string = $(this).data("tessellate-altimg");
+                    var alt: string = (typeof as !== "undefined" && as !== null && as !== "" && s.lastIndexOf("/") !== -1) ? s.replace(s.substring(s.lastIndexOf("/") + 1), $(this).data("tessellate-altimg")) : s;
+                    var tt: bool = $(this).data("tessellate-type") === "fixed";
+                    var ts: number = parseInt($(this).data("tessellate-size"), 10);
+                    var sz: number = (isNaN(ts) || ts === 0) ? ts = (tt ? 20 : 5) : ts;
+                    var squareSize: number = tt ? sz : ((h < w) ? Math.ceil(h / sz) : Math.ceil(w / sz));
+                    var baseSpeed: number = 30;
+                    var accelerator: number = 10;
 
-                    $(this).wrap("<div class=\"tessellated\" style=\"height: " + h + "px; width: " + w + "px;\"></div>");
+                    $(this).wrap("<div class=\"tessellated\" style=\"height: " + h + "px; width: " + w + "px;\" data-tessellate-srcimg=\"" + alt + "\" data-tessellate-altimg=\"" + s + "\"></div>");
                     var wrapper = $(this).parent();
-                    var delayStart = 0, delay;
+                    var delayStart = 0, delay = 0, x = 0;
 
-                    for (var i = 0; i < h; i += 20) {
-                        delay = 0;
-                        for (var j = 0; j < w; j += 20) {
-                            wrapper.append($("<div class=\"tile\" style=\"background-image: url(" + s + "); background-position: -" + j + "px -" + i + "px; -webkit-animation-delay:" + (delayStart + delay) + "ms;\"></div>"));
-                            delay += 50;
+                    for (var i = 0; i < h; i += squareSize, x += accelerator, delay = 0, delayStart += baseSpeed + x) {
+                        for (var j = 0; j < w; j += squareSize, delay += baseSpeed) {
+                            wrapper.append($("<div class=\"tile\" style=\"background-image: url(" + s + "); background-position: -" + j + "px -" + i + "px; width: " + squareSize + "px; height: " + squareSize + "px; -webkit-animation-delay:" + (delayStart + delay) + "ms;\"></div>"));
                         }
                         wrapper.append("<br/>");
-                        delayStart += 50 + i;
                     }
                     $(this).remove();
                 });
@@ -38,13 +43,29 @@ module DangerZone {
 
                     if ($(this).css("-webkit-animation-name") === "flipback") {
                         $(this).addClass("nodelay");
-                        $(this).css({ "-webkit-animation-play-state": "running" });
+                        $(this).css({
+                            "background-image": "url(" + $(this).parent().data("tessellate-altimg") + ")"
+                            , "-webkit-animation-play-state": "running"
+                        });
+                        $(this).parent()
+                    }
+                    else
+                    {
+                        //$(this).css({
+                        //    "background-image": "url(" + $(this).parent().data("tessellate-srcimg") + ")"
+                        //});
                     }
                 });
             });
         }
 
         Flip(selector: string) {
+            $(".tessellated").each(function (idx, obj) {
+                var a: string = $(this).data("tessellate-altimg");
+                $(this).data("tessellate-altimg", $(this).data("tessellate-srcimg"));
+                $(this).data("tessellate-srcimg", a);
+            });
+
             var s = (typeof selector !== "undefined" && selector !== null && selector !== "") ? selector + ">div.tile" : "div.tile";
             $(s).removeClass("nodelay");
             $(s).css({
